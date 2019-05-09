4e89aa969c2c5f9e3d6e146cdd1f1afa5097d19c
MXuDong <2941884109@qq.com>
2019-05-09 21:55:13 +0800

    创建异常类

---
+++ src/main/java/exceptions/ReflectionException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : ReflectionException
+ * Create Time : 21:21
+ * Create Date : 2019/5/9
+ * Project : ReflectionSupport
+ * <p>
+ * ReflectionException
+ */
+
+public class ReflectionException extends Exception {
+    public ReflectionException(String msg, String field){
+        super(msg + "[" + field + "]");
+    }
+}
