0911e7f40f4b794015d40e8879a2da3fd3dee86c
MXuDong <2941884109@qq.com>
2019-06-11 16:45:58 +0800

    添加ClassUtil

---
+++ src/main/java/io/github/mxudong/rs/base/utils/ClassUtil.java
@@ -0,0 +1,26 @@
+package io.github.mxudong.rs.base.utils;
+
+/**
+ * Class Name : ClassUtil
+ * Create Time : 16:44
+ * Create Date : 2019/6/11
+ * Project : ReflectionSupport
+ *
+ * support some static methods for check class.
+ *
+ * @author Dong
+ * @since 2.0
+ */
+
+public class ClassUtil {
+
+    /**
+     * is type base type(byte, short, int, long, char, boolean, float, double)
+     *
+     * @param type input type
+     * @return is base type
+     */
+    public static boolean isBaseType(Class type) {
+
+    }
+}
