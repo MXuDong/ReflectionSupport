80e35260fb779630f86cf8769f3a6b21a5c93e63
MXuDong <2941884109@qq.com>
2019-06-16 13:48:55 +0800

    测试完成

--- src/test/java/io/github/mxudong/rs/base/randoms/BaseRandomTest.java
+++ src/test/java/io/github/mxudong/rs/base/randoms/BaseRandomTest.java
@@ -7,17 +7,50 @@ import static org.junit.Assert.*;
 public class BaseRandomTest {
 
     @Test
-    public void getNextInt() {
-        for(int i = 0; i < 10; i++){
-            System.out.println(BaseRandom.getNextInt(10, 13));
-        }
+    public void getNextByte() {
+        System.out.println("B1++++=========================");
+        System.out.println(BaseRandom.getNextByte());
     }
 
     @Test
-    public void getNextLong() {
-        for(int i = 0; i < 10; i++){
-            long test1 = BaseRandom.getNextLong(0, 1);
-            System.out.println(test1);
-        }
+    public void getNextByte1() {
+        System.out.println("B2++++=========================");
+        System.out.println(BaseRandom.getNextByte((byte) 1, (byte) 5));
+    }
+
+    @Test
+    public void getNextShort() {
+        System.out.println("S1++++=========================");
+        System.out.println(BaseRandom.getNextShort());
+    }
+
+    @Test
+    public void getNextShort1() {
+        System.out.println("S2++++=========================");
+        System.out.println(BaseRandom.getNextShort((short) 10, (short) 15));
+    }
+
+    @Test
+    public void getNextInt1() {
+        System.out.println("I1++++=========================");
+        System.out.println(BaseRandom.getNextInt());
+    }
+
+    @Test
+    public void getNextInt2() {
+        System.out.println("I2++++===================321======");
+        System.out.println(BaseRandom.getNextInt(20, 22));
+    }
+
+    @Test
+    public void getNextLong1() {
+        System.out.println("L1++++=========================");
+        System.out.println(BaseRandom.getNextLong());
+    }
+
+    @Test
+    public void getNextLong2() {
+        System.out.println("L2++++=========================");
+        System.out.println(BaseRandom.getNextLong(75L, 86L));
     }
 }
