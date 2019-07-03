290c2ff8fb6528da0f3dd3b73812b4bf855205c6
MXuDong <2941884109@qq.com>
2019-07-03 17:20:01 +0800

    add class : CommonMethod

---
+++ src/main/java/io/github/mxudong/rs/packings/methods/CommonMethod.java
@@ -0,0 +1,14 @@
+package io.github.mxudong.rs.packings.methods;
+
+/**
+ * this class is packing the common method.
+ *
+ * common methods do not include constructors,
+ * static methods, Setter Method, and Getter Method.
+ *
+ * @author Dong
+ * @since 3.0
+ */
+
+public class CommonMethod {
+}
