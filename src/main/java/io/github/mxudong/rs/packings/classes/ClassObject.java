255f0b0bff436fe0ecf9d9cb231d7cfc93616979
MXuDong <2941884109@qq.com>
2019-07-03 21:55:29 +0800

    add class : ClassObject<T>

---
+++ src/main/java/io/github/mxudong/rs/packings/classes/ClassObject.java
@@ -0,0 +1,12 @@
+package io.github.mxudong.rs.packings.classes;
+
+/**
+ * this class packing the class, and for this class, you
+ * only create from ClassObjectFactor
+ *
+ * @author Dong
+ * @since 3.0
+ */
+
+public class ClassObject<T> {
+}
