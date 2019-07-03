60a43f204df2c5d85b5afd61e4c45abef03202e3
MXuDong <2941884109@qq.com>
2019-07-03 16:41:57 +0800

    add abstract method : getMethodType();

--- src/main/java/io/github/mxudong/rs/packings/methods/Invoker.java
+++ src/main/java/io/github/mxudong/rs/packings/methods/Invoker.java
@@ -14,6 +14,16 @@ public interface Invoker {
      * @param target the aim target of invoking method
      * @param params the parameters required for the method to be invoked
      * @return the result of invoke
+     * @since 3.0
      */
     Object invoke(Object target, Object... params);
+
+    /**
+     * get the method's type
+     *
+     * @return the method's type
+     * @see MethodType
+     * @since 3.0
+     */
+    MethodType getMethodType();
 }
