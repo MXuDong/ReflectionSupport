81b0de6ea28068367ac142cd569927d08fd8b84a
MXuDong <2941884109@qq.com>
2019-07-03 16:33:02 +0800

    add abstract method : Object invoke(Object target, Object ... params);

--- src/main/java/io/github/mxudong/rs/packings/methods/Invoker.java
+++ src/main/java/io/github/mxudong/rs/packings/methods/Invoker.java
@@ -6,4 +6,14 @@ package io.github.mxudong.rs.packings.methods;
  * @author Dong
  */
 public interface Invoker {
+
+    /**
+     * the method {@code object invoke(Object target, Object ... params} will invoke method
+     * which is packing.
+     *
+     * @param target the aim target of invoking method
+     * @param params the parameters required for the method to be invoked
+     * @return the result of invoke
+     */
+    Object invoke(Object target, Object... params);
 }
