ad9dad582ec26e57d0e907d1a435290de7c82dc7
MXuDong <2941884109@qq.com>
2019-07-03 21:29:47 +0800

    CommonMethod implements Invoker

--- src/main/java/io/github/mxudong/rs/packings/methods/CommonMethod.java
+++ src/main/java/io/github/mxudong/rs/packings/methods/CommonMethod.java
@@ -2,7 +2,7 @@ package io.github.mxudong.rs.packings.methods;
 
 /**
  * this class is packing the common method.
- *
+ * <p>
  * common methods do not include constructors,
  * static methods, Setter Method, and Getter Method.
  *
@@ -10,5 +10,34 @@ package io.github.mxudong.rs.packings.methods;
  * @since 3.0
  */
 
-public class CommonMethod {
+public class CommonMethod implements Invoker{
+    @Override
+    public Object invoke(Object target, Object... params) {
+        return null;
+    }
+
+    @Override
+    public MethodType getMethodType() {
+        return null;
+    }
+
+    @Override
+    public boolean isParamsIsThisMethod(Object... params) {
+        return false;
+    }
+
+    @Override
+    public Class[] getMethodParamsType() {
+        return new Class[0];
+    }
+
+    @Override
+    public Class getBelongClass() {
+        return null;
+    }
+
+    @Override
+    public Class<?> getReturnType() {
+        return null;
+    }
 }
