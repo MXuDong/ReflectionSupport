22fe07a9553f2a4e9ccd73e80db8073723c3943a
MXuDong <2941884109@qq.com>
2019-07-03 21:51:05 +0800

    add class : Reflector<T>

---
+++ src/main/java/io/github/mxudong/rs/Reflector.java
@@ -0,0 +1,12 @@
+package io.github.mxudong.rs;
+
+//it is writing
+
+/**
+ * it has packing the object operation of reflection
+ *
+ * @author Dong
+ * @since 3.0
+ */
+public class Reflector<T> {
+}
