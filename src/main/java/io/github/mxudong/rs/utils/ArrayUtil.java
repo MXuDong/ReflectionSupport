339a6621507308fa63c27b49efd2e6faaf6df9f4
MXuDong <2941884109@qq.com>
2019-07-03 22:29:16 +0800

    add static method - ArrayUtil : getArrayClasses(Object... params);

--- src/main/java/io/github/mxudong/rs/utils/ArrayUtil.java
+++ src/main/java/io/github/mxudong/rs/utils/ArrayUtil.java
@@ -10,6 +10,26 @@ package io.github.mxudong.rs.utils;
 public class ArrayUtil {
 
     /**
+     * to get the item class of an array
+     *
+     * @param params the array which will be got classes
+     * @return the classes
+     * @since 3.0
+     */
+    public static Class<?>[] getArrayClasses(Object... params) {
+        if (params == null) {
+            return new Class<?>[]{};
+        }
+
+        Class<?>[] result = new Class[params.length];
+        for (int i = 0; i < params.length; i++) {
+            result[i] = params[i].getClass();
+        }
+
+        return result;
+    }
+
+    /**
      * to equals two arrays is equal
      *
      * @param first  first array
