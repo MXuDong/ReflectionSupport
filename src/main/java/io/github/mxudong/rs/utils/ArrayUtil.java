cf51aa7dd497015564e87553c8f159c308eda53b
MXuDong <2941884109@qq.com>
2019-07-03 22:19:14 +0800

    add static method - ArrayUtil : public static boolean equalsArray(Object [] first, Object [] second);

--- src/main/java/io/github/mxudong/rs/utils/ArrayUtil.java
+++ src/main/java/io/github/mxudong/rs/utils/ArrayUtil.java
@@ -9,5 +9,29 @@ package io.github.mxudong.rs.utils;
 
 public class ArrayUtil {
 
+    /**
+     * to equals two arrays is equal
+     *
+     * @param first  first array
+     * @param second second array
+     * @return if equals return true, else return false
+     * @since 3.0
+     */
+    public static boolean equalsArray(Object[] first, Object[] second) {
+        if (first == null || second == null) {
+            return first == null && second == null;
+        }
 
+        if (first.length != second.length) {
+            return false;
+        }
+
+        for (int i = 0; i < first.length; i++) {
+            if (!first[i].equals(second[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
