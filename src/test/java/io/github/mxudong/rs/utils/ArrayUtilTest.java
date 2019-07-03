fb53716ad3f14c15c387cc4d1b8efb5028fee25d
MXuDong <2941884109@qq.com>
2019-07-03 22:49:35 +0800

    prefect test

--- src/test/java/io/github/mxudong/rs/utils/ArrayUtilTest.java
+++ src/test/java/io/github/mxudong/rs/utils/ArrayUtilTest.java
@@ -5,12 +5,37 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 public class ArrayUtilTest {
 
     @Test
     public void getArrayClasses() throws NoSuchMethodException {
         System.out.println(Arrays.toString(CommonMethod.class.getMethod("getMethodType").getParameterTypes()));
+    }
+
+    @Test
+    public void equalsArray() {
+        Object[] first1 = new Object[]{null};
+        Object[] second1 = new Object[]{};
+        System.out.println(ArrayUtil.equalsArray(first1, second1));
+
+        Object[] first2 = new Object[]{};
+        Object[] second2 = new Object[]{};
+        System.out.println(ArrayUtil.equalsArray(first2, second2));
+
+        Object[] first3 = new Object[]{null};
+        Object[] second3 = new Object[]{null};
+        System.out.println(ArrayUtil.equalsArray(first3, second3));
+
+        Object[] first4 = new Object[]{null};
+        Object[] second4 = new Object[]{1};
+        System.out.println(ArrayUtil.equalsArray(first4, second4));
+
+        Object[] first5 = new Object[]{1,2,3};
+        Object[] second5 = new Object[]{1,2,3};
+        System.out.println(ArrayUtil.equalsArray(first5, second5));
+
+        Object[] first6 = new Object[]{3,2,1};
+        Object[] second6 = new Object[]{1,2,3};
+        System.out.println(ArrayUtil.equalsArray(first6, second6));
     }
 }
