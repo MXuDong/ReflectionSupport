10dffe4a29320b5777de7aa2d8860c9207f3df00
MXuDong <2941884109@qq.com>
2019-07-03 16:27:44 +0800

    添加invoker接口

---
+++ src/main/java/io/github/mxudong/rs/packings/methods/Invoker.java
@@ -0,0 +1,9 @@
+package io.github.mxudong.rs.packings.methods;
+
+/**
+ * it marked the class is packing of {@code method}, and it can be invoked.
+ *
+ * @author Dong
+ */
+public interface Invoker {
+}
