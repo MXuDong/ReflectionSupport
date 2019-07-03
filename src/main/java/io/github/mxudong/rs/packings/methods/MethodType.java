81385147702e4b4a3ec54668f0a4f97cd5a5c43f
MXuDong <2941884109@qq.com>
2019-07-03 16:48:40 +0800

    prefect info - Enum : MethodType

--- src/main/java/io/github/mxudong/rs/packings/methods/MethodType.java
+++ src/main/java/io/github/mxudong/rs/packings/methods/MethodType.java
@@ -4,4 +4,27 @@ package io.github.mxudong.rs.packings.methods;
  * @author Dong
  */
 public enum MethodType {
+
+    /**
+     * ConstructionMethod : the construction method
+     * CommonMethod : the common method
+     * StaticMethod : the static method
+     * GetterMethod : the getter method
+     * SetterMethod : the setter method
+     */
+    ConstructionMethod("Construction"),
+    CommonMethod("Common"),
+    StaticMethod("Static"),
+    GetterMethod("Getter"),
+    SetterMethod("Setter");
+
+    String type;
+
+    MethodType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type + " Method";
+    }
 }
