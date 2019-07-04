2716cbe2094b0039bcf0069f365e0dab868c19c5
MXuDong <2941884109@qq.com>
2019-07-04 08:27:57 +0800

    add static method - ClassUtil : isBasePackingType(Class Type);

--- src/main/java/io/github/mxudong/rs/utils/ClassUtil.java
+++ src/main/java/io/github/mxudong/rs/utils/ClassUtil.java
@@ -14,6 +14,7 @@ public class ClassUtil {
      *
      * @param type aim class
      * @return if class is base type's class, it will return true, else return false
+     * @since 3.0
      */
     public static boolean isBaseType(Class type) {
         if (type.equals(int.class)) {
@@ -38,5 +39,36 @@ public class ClassUtil {
             return true;
         }
         return type.equals(char.class);
+    }
+
+    /**
+     * to judge that a type is packing of base type
+     *
+     * @param type aim class
+     * @return if the aim class is packing of base type, will return true, else return false
+     */
+    public static boolean isBasePackingType(Class type) {
+        if (type.equals(Integer.class)) {
+            return true;
+        }
+        if (type.equals(Short.class)) {
+            return true;
+        }
+        if (type.equals(Byte.class)) {
+            return true;
+        }
+        if (type.equals(Long.class)) {
+            return true;
+        }
+        if (type.equals(Float.class)) {
+            return true;
+        }
+        if (type.equals(Double.class)) {
+            return true;
+        }
+        if (type.equals(Boolean.class)) {
+            return true;
+        }
+        return type.equals(Character.class);
     }
 }
