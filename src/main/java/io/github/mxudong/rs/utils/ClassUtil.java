9a42da68eaa28b8515e3805dfd0d9321346d468f
MXuDong <2941884109@qq.com>
2019-07-04 08:24:46 +0800

    add class : ClassUtil

--- src/main/java/io/github/mxudong/rs/utils/ClassUtil.java
+++ src/main/java/io/github/mxudong/rs/utils/ClassUtil.java
@@ -8,4 +8,35 @@ package io.github.mxudong.rs.utils;
  */
 
 public class ClassUtil {
+
+    /**
+     * to judge a class is base type's class
+     *
+     * @param type aim class
+     * @return if class is base type's class, it will return true, else return false
+     */
+    public static boolean isBaseType(Class type) {
+        if (type.equals(int.class)) {
+            return true;
+        }
+        if (type.equals(short.class)) {
+            return true;
+        }
+        if (type.equals(byte.class)) {
+            return true;
+        }
+        if (type.equals(long.class)) {
+            return true;
+        }
+        if (type.equals(float.class)) {
+            return true;
+        }
+        if (type.equals(double.class)) {
+            return true;
+        }
+        if (type.equals(boolean.class)) {
+            return true;
+        }
+        return type.equals(char.class);
+    }
 }
