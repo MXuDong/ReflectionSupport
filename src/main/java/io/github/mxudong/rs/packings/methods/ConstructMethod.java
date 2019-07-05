0f8dd4ec449f077673b979e3d03af15ac1b5fe19
MXuDong <2941884109@qq.com>
2019-07-05 16:02:07 +0800

    prefect ConstructMethod

--- src/main/java/io/github/mxudong/rs/packings/methods/ConstructMethod.java
+++ src/main/java/io/github/mxudong/rs/packings/methods/ConstructMethod.java
@@ -1,6 +1,10 @@
 package io.github.mxudong.rs.packings.methods;
 
+import io.github.mxudong.rs.packings.classes.ClassObject;
+import io.github.mxudong.rs.utils.MethodUtil;
+
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * this class packing the construction method, so you
@@ -10,47 +14,86 @@ import java.lang.reflect.Constructor;
  * @since 3.0
  */
 
-public class ConstructMethod<T> implements Invoker{
+public class ConstructMethod<T> {
 
     /**
      * the packing constructor
      */
     private Constructor<T> constructor;
 
+    /**
+     * if want to invoke the construction, the params is required
+     */
+    private Class<?>[] paramClasses;
+
+    /**
+     * the class which ClassObject belong to
+     */
+    private ClassObject<T> belongClassObject;
+
+    private final static MethodType METHOD_TYPE = MethodType.ConstructionMethod;
 
 
-    @Override
-    public Object invoke(Object target, Object... params) {
-        return null;
+    /**
+     * invoke the construction method
+     *
+     * @param params invoke the construction needed params
+     * @return new instance
+     * @since 3.0
+     */
+    public Object newInstance(Object... params) {
+        Object result = null;
+        try {
+            result = constructor.newInstance(params);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
-    @Override
+
+    /**
+     * get the method type
+     *
+     * @return method type
+     * @see MethodType
+     * @since 3.0
+     */
     public MethodType getMethodType() {
-        return null;
+        return METHOD_TYPE;
     }
 
-    @Override
+    /**
+     * determine whether the incoming parameters are the required parameters for
+     * this method
+     *
+     * @param params be judge params
+     * @return if the params is packing method required param type
+     * @since 3.0
+     */
     public boolean isParamsIsThisMethod(Object... params) {
-        return false;
+        return MethodUtil.isParamEquals(this.paramClasses, params);
     }
 
-    @Override
-    public Class[] getMethodParamsType() {
-        return new Class[0];
+
+    /**
+     * get the params type
+     *
+     * @return classes from param
+     */
+    public Class<?>[] getMethodParamsType() {
+        return paramClasses;
     }
 
-    @Override
+
+    /**
+     * get the class of packaging method attribution
+     *
+     * @return the class which packing method belong to
+     * @since 3.0
+     */
     public Class getBelongClass() {
-        return null;
-    }
-
-    @Override
-    public Class<?> getReturnType() {
-        return null;
-    }
-
-    @Override
-    public String getMethodName() {
-        return null;
+        return belongClassObject.getPackingClass();
     }
 }
