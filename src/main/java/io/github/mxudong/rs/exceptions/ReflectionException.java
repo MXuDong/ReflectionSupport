571c3cad075b4086f439d0604239a85b6cc63eb1
MXuDong <2941884109@qq.com>
2019-07-06 17:08:51 +0800

    add exception

---
+++ src/main/java/io/github/mxudong/rs/exceptions/ReflectionException.java
@@ -0,0 +1,13 @@
+package io.github.mxudong.rs.exceptions;
+
+/**
+ * this exception will show how wrong you did.
+ *
+ * this exception is the base exception.
+ *
+ * @author Dong
+ * @since 3.0
+ */
+
+public class ReflectionException extends Exception{
+}
