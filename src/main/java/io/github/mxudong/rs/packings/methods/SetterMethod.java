418381ee706743ee5cbb7c99c29c153bbf56ab09
MXuDong <2941884109@qq.com>
2019-07-06 17:29:13 +0800

    add class SetterMethod

---
+++ src/main/java/io/github/mxudong/rs/packings/methods/SetterMethod.java
@@ -0,0 +1,45 @@
+package io.github.mxudong.rs.packings.methods;
+
+import io.github.mxudong.rs.packings.classes.ClassObject;
+
+import java.lang.reflect.Method;
+
+/**
+ * packing the setter method
+ *
+ * @author Dong
+ * @since 3.0
+ */
+
+public class SetterMethod extends CommonMethod {
+
+    private final static MethodType METHOD_TYPE = MethodType.SetterMethod;
+
+    /**
+     * method of {@code CommonMethod}, in this part,
+     * the method will be append for packingMethod.
+     * <p>
+     * the method info will be set in this part
+     *
+     * @param packingMethod the packing method
+     * @param classObject   method belong class object
+     */
+    public SetterMethod(Method packingMethod, ClassObject<?> classObject) {
+        super(packingMethod, classObject);
+    }
+
+    /**
+     * invoke the setter method
+     *
+     * @param target aim object
+     * @return the result of invoke
+     */
+    public Object invoke(Object target) {
+        return super.invoke(target);
+    }
+
+    @Override
+    public MethodType getMethodType() {
+        return METHOD_TYPE;
+    }
+}
