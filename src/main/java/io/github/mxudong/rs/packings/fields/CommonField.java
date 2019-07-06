722f2b10866dc4eed505fa36505df1d9a8ea4e4c
MXuDong <2941884109@qq.com>
2019-07-06 23:24:52 +0800

    add setter and getter method

--- src/main/java/io/github/mxudong/rs/packings/fields/CommonField.java
+++ src/main/java/io/github/mxudong/rs/packings/fields/CommonField.java
@@ -1,7 +1,16 @@
 package io.github.mxudong.rs.packings.fields;
 
+import io.github.mxudong.rs.packings.classes.ClassObject;
+import io.github.mxudong.rs.packings.methods.GetterMethod;
+import io.github.mxudong.rs.packings.methods.SetterMethod;
+
+import java.lang.reflect.Field;
+
 /**
- * packing the field in class
+ * the CommonField packing the Field in class, and
+ * provides method to manage Field
+ * <p>
+ * for every field, will check setterMethod and check getterMethod
  *
  * @author Dong
  * @since 3.0
@@ -9,4 +18,34 @@ package io.github.mxudong.rs.packings.fields;
 
 public class CommonField {
 
+    /**
+     * the packing field
+     */
+    private Field packingField;
+
+    /**
+     * the class which has packing field
+     */
+    private ClassObject<?> classObject;
+
+    /**
+     * the getter method of this field
+     */
+    private GetterMethod fieldGetterMethod;
+
+    /**
+     * the setter method of this field
+     */
+    private SetterMethod fieldSetterMethod;
+
+    /**
+     * construction method, in this method, filed info will be save
+     *
+     * @param packingField packing field
+     * @param classObject  the class which packing field from
+     */
+    public CommonField(Field packingField, ClassObject<?> classObject) {
+        this.packingField = packingField;
+        this.classObject = classObject;
+    }
 }
