4d6183cd1095a00bf0bf080922e63c72f4e838ab
MXuDong <2941884109@qq.com>
2019-07-06 19:02:47 +0800

    fix ReflectionException

--- src/main/java/io/github/mxudong/rs/exceptions/ReflectionException.java
+++ src/main/java/io/github/mxudong/rs/exceptions/ReflectionException.java
@@ -2,12 +2,20 @@ package io.github.mxudong.rs.exceptions;
 
 /**
  * this exception will show how wrong you did.
- *
+ * <p>
  * this exception is the base exception.
+ * <p>
+ * the exception format is : [info] [in [className].[methodName]]
  *
  * @author Dong
  * @since 3.0
  */
 
-public class ReflectionException extends Exception{
+public class ReflectionException extends Exception {
+
+    public ReflectionException(String className, String methodName, String info) {
+        super("[" + info + "]" +
+                "[in [" + className + "].["
+                + methodName + "]]");
+    }
 }
