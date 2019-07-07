da83151f5f50b4b8024688dd58b70618828d5b58
MXuDong <2941884109@qq.com>
2019-07-07 19:45:44 +0800

    add class

---
+++ src/main/java/io/github/mxudong/rs/packings/classes/InterfaceObject.java
@@ -0,0 +1,26 @@
+package io.github.mxudong.rs.packings.classes;
+
+/**
+ * class InterfaceObject packing the interface info, this class
+ * provides some method to manage interface info
+ *
+ * @author Dong
+ * @since 3.0
+ */
+
+public class InterfaceObject<T> {
+
+    /**
+     * the class of packing interface
+     */
+    private Class<T> interfaceClass;
+
+    /**
+     * construction method
+     * @param interfaceObject packing interface
+     */
+    public InterfaceObject(Class<T> interfaceObject) {
+        this.interfaceClass = interfaceObject;
+
+    }
+}
