6ff8b47237401bb539139c84da5b1a23493a9ea4
MXuDong <2941884109@qq.com>
2019-07-09 21:32:24 +0800

    add Test

---
+++ src/test/java/io/github/mxudong/rs/ReflectorTest.java
@@ -0,0 +1,18 @@
+package io.github.mxudong.rs;
+
+import io.github.mxudong.rs.bean.Man;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ReflectorTest {
+
+    @Test
+    public void test1(){
+        Reflector<Man> manReflector = new Reflector<>(new Man());
+        System.out.println(Arrays.toString(manReflector.getClassObject().getAllmethods()));
+    }
+
+}
