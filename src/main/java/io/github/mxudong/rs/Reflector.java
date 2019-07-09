9b3b0e044c0f2e73518cf2ec1c434cc865530b9b
MXuDong <2941884109@qq.com>
2019-07-09 21:31:06 +0800

    add infos of Reflector

--- src/main/java/io/github/mxudong/rs/Reflector.java
+++ src/main/java/io/github/mxudong/rs/Reflector.java
@@ -2,6 +2,9 @@ package io.github.mxudong.rs;
 
 //it is writing
 
+import io.github.mxudong.rs.packings.classes.ClassObject;
+import io.github.mxudong.rs.packings.classes.ObjectFactory;
+
 /**
  * it has packing the object operation of reflection
  *
@@ -9,4 +12,46 @@ package io.github.mxudong.rs;
  * @since 3.0
  */
 public class Reflector<T> {
+
+    /**
+     * packing the classObject
+     *
+     * @see ClassObject
+     */
+    private ClassObject<T> classObject;
+
+    /**
+     * packing the object
+     */
+    private T object;
+
+    /**
+     * the object factory
+     *
+     * @see ObjectFactory
+     */
+    ObjectFactory objectFactory = ObjectFactory.getInstance();
+
+    public Reflector(T object) {
+        classObject = (ClassObject<T>) objectFactory.getClassObject(object.getClass());
+        this.object = object;
+    }
+
+    /**
+     * get the classObject
+     *
+     * @return ClassObject
+     */
+    public ClassObject<T> getClassObject() {
+        return classObject;
+    }
+
+    /**
+     * get the inner object
+     *
+     * @return inner object
+     */
+    public T getInnerObject() {
+        return object;
+    }
 }
