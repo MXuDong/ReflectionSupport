dbaaed2b3bb66363f2903f72787e043890dde937
MXuDong <2941884109@qq.com>
2019-07-12 11:45:49 +0800

    add interface

---
+++ src/main/java/io/github/mxudong/rs/packings/classes/AnnotationAble.java
@@ -0,0 +1,34 @@
+package io.github.mxudong.rs.packings.classes;
+
+import java.util.List;
+
+/**
+ * AnnotationAble interface provide some method to
+ * operation annotation of this class
+ *
+ * if implement this interface, that mark the class can
+ * operation annotation, and in this class, it must save the annotation info
+ *
+ * @author Dong
+ */
+public interface AnnotationAble {
+
+    /**
+     * get the annotation info count
+     * @return the count of annotation info
+     */
+    int getAnnotationCount();
+
+    /**
+     * get the annotation object
+     * @param annotationClass target annotation class
+     * @return annotationObject
+     */
+    AnnotationObject getAnnotation(Class annotationClass);
+
+    /**
+     * get the all annotation object
+     * @return list of annotation object
+     */
+    List<AnnotationObject> getAllAnnotation();
+}
