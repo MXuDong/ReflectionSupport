46b296036381cff8ad777aefc554385614e2c393
MXuDong <2941884109@qq.com>
2019-07-14 22:29:25 +0800

    add the stringutil

---
+++ src/main/java/io/github/mxudong/rs/utils/StringUtil.java
@@ -0,0 +1,20 @@
+package io.github.mxudong.rs.utils;
+
+/**
+ * provide some operation about string
+ *
+ * @author Dong
+ * @since 3.0
+ */
+
+public class StringUtil {
+
+    public static final String UPPER_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    public static final String NUMBER_LETTERS = "0123456789";
+    public static final String OTHER_TYPE_LETTERS = "{}[]()<>,.':;?/=+-*&|!^%#@~`\"\\";
+    public static final char SPACE_CHAR = ' ';
+    public static final char DOLLAR_CHAR = '$';
+    public static final char UNDER_LINE_CHAR = '_';
+    public static final char POINT_CHAR = '.';
+}
