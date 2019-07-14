7779cfa43b19be39319e85d03fc72195d3d5f7ab
MXuDong <2941884109@qq.com>
2019-07-14 23:18:13 +0800

    add construction method

--- src/main/java/io/github/mxudong/rs/randoms/Randomizer.java
+++ src/main/java/io/github/mxudong/rs/randoms/Randomizer.java
@@ -1,9 +1,37 @@
 package io.github.mxudong.rs.randoms;
 
+import io.github.mxudong.rs.Reflector;
+
 /**
+ * the randomizer is for random any object,
+ * you can use this create an object in random.
+ *
  * @author Dong
  * @since 3.0
  */
 
-public class Randomizer {
+public class Randomizer<T> {
+
+    /**
+     * packing the object in reflector
+     */
+    private Reflector<T> packingObjectReflector;
+
+    /**
+     * construct method
+     *
+     * @param object target object
+     */
+    public Randomizer(T object) {
+        packingObjectReflector = new Reflector<>(object);
+    }
+
+    /**
+     * construct method
+     *
+     * @param tClass target object's classes
+     */
+    public Randomizer(Class<T> tClass) {
+        packingObjectReflector = new Reflector<>(tClass);
+    }
 }
