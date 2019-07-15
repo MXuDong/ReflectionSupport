347887042ed0ac88e473574fd270cfe604029dc8
MXuDong <2941884109@qq.com>
2019-07-15 21:25:21 +0800

    fix document

--- src/main/java/io/github/mxudong/rs/randoms/annotations/IntegerValue.java
+++ src/main/java/io/github/mxudong/rs/randoms/annotations/IntegerValue.java
@@ -3,7 +3,15 @@ package io.github.mxudong.rs.randoms.annotations;
 import java.lang.annotation.*;
 
 /**
+ * specify the generated Integer data value
+ * <p>
+ * so it only affects Integer type data, and it has no impact on any other type of data.
+ * <p>
+ * priority is higher than Random Limit and Integer Limit
+ *
  * @author Dong
+ * @see IntegerLimit
+ * @see RandomLimit
  */
 
 @Documented
