2a62f1188bf351208fd29a8ccfbbf48ad6bf1383
MXuDong <2941884109@qq.com>
2019-07-15 21:23:17 +0800

    fix document

--- src/main/java/io/github/mxudong/rs/randoms/annotations/IntegerLimit.java
+++ src/main/java/io/github/mxudong/rs/randoms/annotations/IntegerLimit.java
@@ -3,9 +3,15 @@ package io.github.mxudong.rs.randoms.annotations;
 import java.lang.annotation.*;
 
 /**
- * to limit integer create over
+ * Restricting Integer's Generation Interval
+ * <p>
+ * so it only affects Integer type data, and it has no impact on any other type of data.
+ * <p>
+ * and priority lower than IntegerValue but high then RandomLimit
  *
  * @author Dong
+ * @see RandomLimit
+ * @see IntegerValue
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
